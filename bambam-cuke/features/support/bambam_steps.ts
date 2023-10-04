4fc4b06bb9ef7b6bc594d52bafc42df40342b5c9
Kyle Krull <kdkrull@gmail.com>
2023-10-04 10:23:18 -0500

    Think thorugh interface a little

--- bambam-cuke/features/support/bambam_steps.ts
+++ bambam-cuke/features/support/bambam_steps.ts
@@ -22,6 +22,7 @@ Then(
   'the General MIDI track should play the same drum pattern as the original',
   () => {
     return 'pending';
+    // expect(generalMidi.noteTimes).to.eql(ezdrummerMidi.noteTimes);
   },
 );
 
@@ -29,5 +30,7 @@ Then(
   'non-standard notes should be changed to their equivalent in General MIDI',
   () => {
     return 'pending';
+    //39 D1 Acoustic Snare; renampped from EZDrummer 2 snare rimshot
+    // expect(generalMidi.noteAt({ measure: 1, beat: 1, tick: 0 })).to.eql(38);
   },
 );
