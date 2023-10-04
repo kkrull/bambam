607471905734ce2b3ab0d03f75909b891f65e19b
Kyle Krull <kdkrull@gmail.com>
2023-10-04 09:27:52 -0500

    Add pending step definitions

---
+++ bambam-cuke/features/support/bambam_steps.ts
@@ -0,0 +1,28 @@
+import assert from 'assert';
+import { Given, Then, When } from '@cucumber/cucumber';
+
+Given('I have exported an EZDrummer 2 track from my DAW, as MIDI', () => {
+  return 'pending';
+});
+
+When('I ask BamBam to convert that track to General MIDI', () => {
+  return 'pending';
+});
+
+Then('BamBam should create a track that plays back at the same tempo', () => {
+  return 'pending';
+});
+
+Then(
+  'the General MIDI track should play the same drum pattern as the original',
+  () => {
+    return 'pending';
+  },
+);
+
+Then(
+  'non-standard notes should be changed to their equivalent in General MIDI',
+  () => {
+    return 'pending';
+  },
+);
