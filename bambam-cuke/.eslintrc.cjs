5dec2da7c1f68abc5c5be263949012324d4bd1c5
Kyle Krull <kdkrull@gmail.com>
2023-10-04 09:55:02 -0500

    Configure eslint to check commonjs files too

--- bambam-cuke/.eslintrc.cjs
+++ bambam-cuke/.eslintrc.cjs
@@ -1,6 +1,9 @@
 /* eslint-env node */
 module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  env: {
+    node: true,
+  },
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
