2712216bf4e76ed62e03463f6e7e4c71b59afeee
Kyle Krull <kkrull@users.noreply.github.com>
2023-10-05 16:21:21 -0500

    Update CI job (#3)

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,10 +1,11 @@
+//Use lambda to avoid error TS5042 (passing project file and sources to check)
+const typeCheckWholeProject = () =>
+  'tsc --noEmit -p tsconfig.json -p features/tsconfig.json';
+
 //https://github.com/okonet/lint-staged#configuration
 module.exports = {
-  '*.cjs': ['eslint --cache --fix', 'prettier --write'],
-  '*.md': ['prettier --write'],
-  '*.ts': [
-    'eslint --cache --fix',
-    'prettier --write',
-    'tsc --noEmit -p tsconfig.json',
-  ],
+  //Format code first, in case that triggers lint errors
+  '*.cjs': ['prettier --write', 'eslint --cache --fix'],
+  '*.md': ['prettier --write', 'markdownlint-cli2 .markdownlint.json'],
+  '*.ts': ['prettier --write', 'eslint --cache --fix', typeCheckWholeProject],
 };
