f1fec58157a326669003a2a0516d6e777b9f8b3a
Kyle Krull <kdkrull@gmail.com>
2023-10-05 16:18:14 -0500

    Clarify why using a lambda

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,11 +1,11 @@
+//Use lambda to avoid error TS5042 (passing project file and sources to check)
+const typeCheckWholeProject = () =>
+  'tsc --noEmit -p tsconfig.json -p features/tsconfig.json';
+
 //https://github.com/okonet/lint-staged#configuration
 module.exports = {
-  //Format code before running the style-based that happen during linting
+  //Format code first, in case that triggers lint errors
   '*.cjs': ['prettier --write', 'eslint --cache --fix'],
   '*.md': ['prettier --write', 'markdownlint-cli2 .markdownlint.json'],
-  '*.ts': [
-    'prettier --write',
-    'eslint --cache --fix',
-    () => 'tsc --noEmit -p tsconfig.json -p features/tsconfig.json',
-  ],
+  '*.ts': ['prettier --write', 'eslint --cache --fix', typeCheckWholeProject],
 };
