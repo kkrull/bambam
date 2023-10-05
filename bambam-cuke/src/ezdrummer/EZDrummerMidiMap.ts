430ede106be4161efff2beced3a7b57de2b287df
Kyle Krull <kdkrull@gmail.com>
2023-10-04 23:48:36 -0500

    Starting to map notes

--- bambam-cuke/src/ezdrummer/EZDrummerMidiMap.ts
+++ bambam-cuke/src/ezdrummer/EZDrummerMidiMap.ts
@@ -4,15 +4,20 @@ import { NoteEvent } from '../midi/events';
 //Produces altered versions of given events, such as transposing or re-mapping
 export default class EZDrummerMidiMap implements MidiMap {
   public static version2Map(): EZDrummerMidiMap {
-    return new EZDrummerMidiMap();
+    return new EZDrummerMidiMap({ [24]: 42 });
   }
 
+  private constructor(
+    private readonly _noteNumberMap: { [key: number]: number },
+  ) {}
+
   remap(event: Readonly<NoteEvent>): NoteEvent {
+    const newNoteNumber = this._noteNumberMap[event.noteNumber];
     //TODO KDK: Implement
     // return event.withNoteNumber(42);
     return {
       when: event.when,
-      noteNumber: 42,
+      noteNumber: newNoteNumber,
       how: event.how,
     };
   }
