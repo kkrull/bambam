37a0a4e03613cc9fb83479e6737177240d412162
Kyle Krull <kdkrull@gmail.com>
2023-10-04 19:36:53 -0500

    Start defining a MIDI note

--- bambam-cuke/features/support/bambam_steps.ts
+++ bambam-cuke/features/support/bambam_steps.ts
@@ -7,7 +7,13 @@ let ezdrummerTrack: MidiTrack;
 let gmTrack: MidiTrack;
 
 Given('I have exported an EZDrummer 2 track from my DAW, as MIDI', () => {
+  //C1 24 (Hats Open 1): No GM mapping
   ezdrummerTrack = new MidiTrack(120);
+  ezdrummerTrack.addNote(
+    24,
+    { measure: 1, beat: 1, tick: 0 },
+    { velocity: 100 },
+  );
 });
 
 When('I ask BamBam to convert that track to General MIDI', () => {
@@ -30,7 +36,7 @@ Then(
   'non-standard notes should be changed to their equivalent in General MIDI',
   () => {
     return 'pending';
-    //39 D1 Acoustic Snare; renampped from EZDrummer 2 snare rimshot
-    // expect(generalMidi.noteAt({ measure: 1, beat: 1, tick: 0 })).to.eql(38);
+    //42 F#1 (Closed Hi Hat)
+    // expect(generalMidi.noteAt({ measure: 1, beat: 1, tick: 0 })).to.eql(42);
   },
 );
--- bambam-cuke/src/midi/MidiTrack.ts
+++ bambam-cuke/src/midi/MidiTrack.ts
@@ -1,6 +1,14 @@
 class MidiTrack {
   constructor(private readonly bpm: number) {}
 
+  addNote(
+    noteNumber: number,
+    when: { measure: number; beat: number; tick: number },
+    how: { velocity: number },
+  ) {
+    throw new Error('Method not implemented.');
+  }
+
   beatsPerMinute(): number {
     return 120;
   }
