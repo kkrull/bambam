66dc75ea03c9ed7d58f6f8e46b4e6959ad023e53
Kyle Krull <kdkrull@gmail.com>
2023-10-06 12:00:30 -0500

    Extract function.  Sweet Jesus

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,15 @@
+const allowUnusedIgnoreVariables = {
+  'no-unused-vars': 'off', //avoid false positives from the base rule
+  '@typescript-eslint/no-unused-vars': [
+    'error',
+    {
+      argsIgnorePattern: '^_',
+      caughtErrorsIgnorePattern: '^_',
+      varsIgnorePattern: '^_',
+    },
+  ],
+};
+
 /* eslint-env node */
 module.exports = {
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
@@ -8,16 +20,7 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   root: true,
   rules: {
+    ...allowUnusedIgnoreVariables,
     'max-len': [1, { code: 100 }],
-    // note you must disable the base rule as it can report incorrect errors
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': [
-      'warn', // or "error"
-      {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-        caughtErrorsIgnorePattern: '^_',
-      },
-    ],
   },
 };
