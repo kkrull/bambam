5db9596a99e9f36123caf4529182fa0e68091811
Kyle Krull <kdkrull@gmail.com>
2023-10-06 10:52:29 -0500

    Rename variable

--- features/support/bambam_steps.ts
+++ features/support/bambam_steps.ts
@@ -4,32 +4,32 @@ import { expect } from 'chai';
 import EZDrummerMidiMap from '@/src/ezdrummer/EZDrummerMidiMap';
 import MidiTrack from '@/src/midi/MidiTrack';
 
-let ezdrummerTrack: MidiTrack;
+let ezDrummerTrack: MidiTrack;
 let gmTrack: MidiTrack;
 
 Given('I have exported an EZDrummer 2 track from my DAW, as MIDI', () => {
   //C0 24 (Hats Open 1): No GM mapping
   //TODO KDK: Add a note that doesn't have to be re-mapped
   //TODO KDK: Add the other notes that have to be re-mapped?
-  ezdrummerTrack = new MidiTrack(120);
-  ezdrummerTrack.addNote({ measure: 1, beat: 1, tick: 0 }, 24, {
+  ezDrummerTrack = new MidiTrack(120);
+  ezDrummerTrack.addNote({ measure: 1, beat: 1, tick: 0 }, 24, {
     velocity: 100,
   });
 });
 
 When('I ask BamBam to remap that track to General MIDI Percussion', () => {
   const midiMap = EZDrummerMidiMap.version2Map();
-  gmTrack = ezdrummerTrack.remap(midiMap);
+  gmTrack = ezDrummerTrack.remap(midiMap);
 });
 
 Then('BamBam should create a track that plays back at the same tempo', () => {
-  expect(gmTrack.beatsPerMinute).to.eql(ezdrummerTrack.beatsPerMinute);
+  expect(gmTrack.beatsPerMinute).to.eql(ezDrummerTrack.beatsPerMinute);
 });
 
 Then(
   'the General MIDI track should play the same drum pattern as the original',
   () => {
-    expect(gmTrack.noteTimes()).to.eql(ezdrummerTrack.noteTimes());
+    expect(gmTrack.noteTimes()).to.eql(ezDrummerTrack.noteTimes());
   },
 );
 
