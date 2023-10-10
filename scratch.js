9c400bd228f7747b8a0b1357a4e3bd32642372f7
Kyle Krull <kdkrull@gmail.com>
2023-10-10 15:25:12 -0500

    Extract helper class

--- scratch.js
+++ scratch.js
@@ -3,27 +3,51 @@ const fs = require('node:fs/promises');
 const { Buffer } = require('node:buffer');
 
 (async () => {
-  console.log('Opening file');
   const fh = await fs.open('./features/data/ezd-mapping.mid', 'r');
 
   const chunkType = await readBytes(fh, 4);
-  console.log(`${chunkType.hex().join(' ')}\t${chunkType.text()}`);
+  console.log(`${chunkType.toHex().join(' ')}\t${chunkType.toText()}`);
 
   const chunkLength = await readBytes(fh, 4);
-  console.log(`Chunk length: ${chunkLength.number()} bytes`);
+  console.log(`Chunk length: ${chunkLength.toNumber()} bytes`);
 
-  const chunkData = await readBytes(fh, chunkLength.number());
-  console.log(chunkData);
+  const chunkData = await readBytes(fh, chunkLength.toNumber());
+  console.log(chunkData.toObject());
 })();
 
 async function readBytes(fh, numBytes) {
   const buffer = Buffer.alloc(numBytes);
   await fh.read(buffer, { length: numBytes });
+  return new MidiData(buffer);
+}
 
-  return {
-    data: [...buffer],
-    hex: () => [...buffer].map((x) => Buffer.from([x]).toString('hex')),
-    number: () => buffer.readInt32BE(0),
-    text: () => buffer.toString('latin1'),
-  };
+class MidiData {
+  constructor(buffer) {
+    this.buffer = buffer;
+  }
+
+  toBytes() {
+    return [...this.buffer];
+  }
+
+  toHex() {
+    return this.toBytes().map((x) => Buffer.from([x]).toString('hex'));
+  }
+
+  toNumber() {
+    return this.buffer.readInt32BE(0);
+  }
+
+  toText() {
+    return this.buffer.toString('latin1');
+  }
+
+  toObject() {
+    return {
+      bytes: this.toBytes(),
+      hex: this.toHex(),
+      number: this.toNumber(),
+      text: this.toText(),
+    };
+  }
 }
