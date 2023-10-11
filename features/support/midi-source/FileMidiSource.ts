146b93fed34744d0f31f7b9c4bc3615fd24e1d9d
Kyle Krull <kdkrull@gmail.com>
2023-10-11 10:14:30 -0500

    Leave entry point when activating tag

--- features/support/midi-source/FileMidiSource.ts
+++ features/support/midi-source/FileMidiSource.ts
@@ -1,11 +1,25 @@
-import { MidiStream } from '@/src/stream/MidiStream';
 import { MidiTrack } from '@/src/midi/track/MidiTrack';
 import { MidiSource } from './MidiSource';
 
 //Reads MIDI from a file
 export class FileMidiSource implements MidiSource {
   readTrack(): MidiTrack {
+    throw new Error('Method not implemented.');
+  }
+
+  readTrackPrototype(): MidiTrack {
     const stream = MidiStream.readFile('../../data/ezd-mapping.mid');
     return stream.readTrack();
+  }
+}
+
+//Reads and parses MIDI data from a byte stream
+class MidiStream {
+  public static readFile(_filename: string): MidiStream {
+    throw new Error('Method not implemented.');
+  }
+
+  readTrack(): MidiTrack {
+    throw new Error('Method not implemented.');
   }
 }
