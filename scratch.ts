4e2d089daa1a5348fbf6e18b04df59f7f22bb26c
Kyle Krull <kdkrull@gmail.com>
2023-10-10 21:47:33 -0500

    Convert to typescript

--- scratch.ts
+++ scratch.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const fs = require('node:fs/promises');
-const { Buffer } = require('node:buffer');
+import { FileHandle, open } from 'node:fs/promises';
+import { Buffer } from 'node:buffer';
 
 (async () => {
-  const fh = await fs.open('./features/data/ezd-mapping.mid', 'r');
+  const fh = await open('./features/data/ezd-mapping.mid', 'r');
+  const stat = await fh.stat();
+  console.log(`File: ${stat.size} bytes`);
 
   //TODO KDK: Read the next chunk then parse the file chunk
   const chunkType = await readBytes(fh, 4);
@@ -16,16 +18,14 @@ const { Buffer } = require('node:buffer');
   console.log(chunkData.toObject());
 })();
 
-async function readBytes(fh, numBytes) {
+async function readBytes(fh: FileHandle, numBytes: number): Promise<MidiData> {
   const buffer = Buffer.alloc(numBytes);
-  await fh.read(buffer, { length: numBytes });
+  await fh.read(buffer, 0, numBytes);
   return new MidiData(buffer);
 }
 
 class MidiData {
-  constructor(buffer) {
-    this.buffer = buffer;
-  }
+  constructor(private buffer: Buffer) {}
 
   toBytes() {
     return [...this.buffer];
