39abff60020da70ec8e1b5b02c15890ca8350e8a
Kyle Krull <kdkrull@gmail.com>
2023-10-14 23:45:20 -0500

    Read header chunk and close file

--- features/support/midi-source/mapping/FileMappingMidiSource.ts
+++ features/support/midi-source/mapping/FileMappingMidiSource.ts
@@ -1,5 +1,6 @@
 import path from 'node:path';
 
+import { openFile, readChunk } from '@/src/midi/track/io';
 import { MidiTrack } from '@/src/midi/track/MidiTrack';
 import { MidiSource } from '@/support/midi-source/MidiSource';
 
@@ -11,6 +12,11 @@ export class FileMappingMidiSource implements MidiSource {
   );
 
   async readTrack(): Promise<MidiTrack> {
-    throw Error('Not implemented');
+    const fh = await openFile(this.midiPath);
+    const headerChunk = await readChunk(fh);
+    console.log(`${headerChunk.typeName}: ${headerChunk.length} bytes`);
+
+    await fh.close();
+    throw Error('not finished');
   }
 }
---
+++ src/midi/track/io.ts
@@ -0,0 +1,106 @@
+import { FileHandle, open } from 'node:fs/promises';
+
+export function openFile(filename: string): Promise<FileHandle> {
+  return open(filename, 'r');
+}
+
+export async function readChunk(file: FileHandle): Promise<MidiChunk> {
+  const chunkType = await MidiData.read(file, 4);
+  if (chunkType.isEmpty()) {
+    return MidiChunk.empty();
+  }
+
+  const chunkLength = await MidiData.read(file, 4);
+  const chunkData = await MidiData.read(file, chunkLength.asInt32());
+  return new MidiChunk(chunkType.asText(), chunkLength.asInt32(), chunkData);
+}
+
+//Top-level structure for MIDI data.
+export class MidiChunk {
+  static empty(): MidiChunk {
+    return new MidiChunk('', 0, MidiData.empty());
+  }
+
+  constructor(
+    public readonly typeName: string,
+    public readonly length: number,
+    public readonly data: MidiData,
+  ) {}
+}
+
+//One or more bytes of MIDI data comprising a single value
+export class MidiData {
+  static empty(): MidiData {
+    return new MidiData(Buffer.alloc(0));
+  }
+
+  static async read(file: FileHandle, nBytes: number): Promise<MidiData> {
+    const buffer = Buffer.alloc(nBytes);
+    const result = await file.read({ buffer, length: nBytes });
+    return new MidiData(result.buffer.subarray(0, result.bytesRead));
+  }
+
+  private offset: number;
+
+  private constructor(private readonly buffer: Buffer) {
+    this.offset = 0;
+  }
+
+  asInt16(): number {
+    return this.buffer.readInt16BE(0);
+  }
+
+  asInt32(): number {
+    return this.buffer.readInt32BE(0);
+  }
+
+  asText(): string {
+    return this.buffer.toString('latin1');
+  }
+
+  isEmpty(): boolean {
+    return this.buffer.length === 0;
+  }
+
+  offsetBuffer(): Buffer {
+    return this.buffer.subarray(this.offset);
+  }
+
+  //Variable-length quantity of 1..4 bytes of 7 bits per byte.
+  //All bytes except the last have bit 7 set; in the last byte it is clear.
+  readQuantity(): number {
+    let quantity = 0;
+    for (const rawByte of this.offsetBuffer()) {
+      this.offset++;
+
+      quantity = (quantity << 7) + (rawByte & 0x7f);
+      if ((rawByte & 0x80) === 0) {
+        break;
+      }
+    }
+
+    return quantity;
+  }
+
+  readUInt8(): number {
+    const bytes = [...this.buffer.subarray(this.offset)];
+    this.offset += 1;
+    return bytes[0];
+  }
+
+  // slice(firstOffset: number, endOffset: number): MidiData {
+  //   return new MidiData(this.buffer.subarray(firstOffset, endOffset));
+  // }
+
+  toBytes(): number[] {
+    return [...this.buffer];
+  }
+
+  toObject() {
+    return {
+      bytes: this.toBytes(),
+      number: this.asInt32(),
+      text: this.asText(),
+    };
+  }
+}
