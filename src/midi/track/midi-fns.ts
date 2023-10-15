a27962cdcb6f885b156a2e0de52000ccf6efdb35
Kyle Krull <kdkrull@gmail.com>
2023-10-15 00:30:41 -0500

    Parse header chunk

--- src/midi/track/midi-fns.ts
+++ src/midi/track/midi-fns.ts
@@ -3,11 +3,28 @@ import { MidiChunk, MidiData } from './MidiChunk';
 
 /* Header chunks */
 
-export function parseHeader(_header: MidiChunk): HeaderChunk {
+function parseDivision(division: number): number {
+  const divisionType = (division & 0x8000) >> 15;
+  if (divisionType === 0) {
+    //Ticks per quarter note
+    return division & 0x7fff;
+  } else {
+    //Sub-divisions of a second, ala SMPTE
+    throw Error(`Unsupported division type ${divisionType} in: ${division}`);
+  }
+}
+
+export function parseHeader(header: MidiChunk): HeaderChunk {
+  if (header.length !== 6) {
+    throw Error(`Expected header to have 6 bytes, but has: ${header.length}`);
+  }
+
   return {
-    format: 1,
-    numTracks: 2,
-    division: { ticksPerQuarterNote: 960 },
+    format: header.data.slice(0, 2).asInt16(),
+    numTracks: header.data.slice(2, 4).asInt16(),
+    division: {
+      ticksPerQuarterNote: parseDivision(header.data.slice(4, 6).asInt16()),
+    },
   };
 }
 
