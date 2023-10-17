1dc8b2ad2d314a952f2d37f754c40cb1afcfe394
Kyle Krull <kdkrull@gmail.com>
2023-10-17 10:50:30 -0500

    List out eventst

--- src/main/list-events.ts
+++ src/main/list-events.ts
@@ -1,3 +1,12 @@
+import { FileHandle } from 'fs/promises';
+import {
+  openFile,
+  parseHeader,
+  readChunk,
+  readEvents,
+} from '../midi/track/midi-fns';
+import { MidiChunk } from '../midi/track/MidiChunk';
+
 class ListEventsCommand {
   static parseArgv(argv: string[]): ListEventsCommand {
     if (argv.length !== 3) {
@@ -8,7 +17,41 @@ class ListEventsCommand {
   }
 
   constructor(readonly filename: string) {}
+
+  async run(): Promise<void> {
+    const file = await openFile(this.filename);
+    await this.listFileEvents(file);
+    await file.close();
+  }
+
+  async listFileEvents(file: FileHandle): Promise<void> {
+    const headerChunk = await readChunk(file);
+    const { format, numTracks, division } = parseHeader(headerChunk);
+    console.log(
+      `Header: format=${format}, numTracks=${numTracks}, division=${JSON.stringify(
+        division,
+        null,
+        2,
+      )}`,
+    );
+
+    let trackChunk = await readChunk(file);
+    while (!trackChunk.isEmpty()) {
+      console.log();
+      console.log('Track');
+      this.listTrackEvents(trackChunk);
+      trackChunk = await readChunk(file);
+    }
+  }
+
+  //TODO KDK: List out the events, to use as a basis for updating StaticMappingMidiSource
+  listTrackEvents(trackChunk: MidiChunk): void {
+    readEvents(trackChunk).forEach((event) => {
+      console.log(JSON.stringify(event, null, 2));
+    });
+  }
 }
 
-ListEventsCommand.parseArgv(process.argv);
-//TODO KDK: List out the events, to use as a basis for updating StaticMappingMidiSource
+(async (command: ListEventsCommand) => {
+  await command.run();
+})(ListEventsCommand.parseArgv(process.argv));
