fc5bce3d02da8dc645dafed964944103e0130f97
Kyle Krull <kdkrull@gmail.com>
2023-10-21 00:22:31 -0500

    Skip pending scenario

--- features/cucumber.cjs
+++ features/cucumber.cjs
@@ -1,7 +1,9 @@
+//https://github.com/cucumber/cucumber-js/blob/main/docs/configuration.md#options
 module.exports = {
   default: {
     format: ['@cucumber/pretty-formatter'],
     require: ['features/**/*.ts'],
     requireModule: ['ts-node/register', 'tsconfig-paths/register'],
+    tags: 'not @skip',
   },
 };
