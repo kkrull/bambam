7637464f45178527df2462d9389f5032d4e69c7f
Kyle Krull <kdkrull@gmail.com>
2023-10-22 13:11:06 -0500

    Try remapping quickly

--- src/main/remap-events.ts
+++ src/main/remap-events.ts
@@ -8,6 +8,11 @@ import {
   writeUInt32,
 } from '@src/midi/io/io-fns';
 import { readEvents } from '@src/midi/track/track-fns';
+import { parseHeader } from '@src/midi/header/header-fns';
+import { MidiTrackBuilder } from '@src/midi/track/MidiTrackBuilder';
+import { EZDrummerMidiMap } from '@src/ezd-mapper/EZDrummerMidiMap';
+import { Division } from '@src/midi/header/HeaderChunk';
+import { MidiTrack } from '@src/midi/track/MidiTrack';
 
 //Copy events from a MIDI file to make sure they are brought to me...unspoiled.
 class RemapEventsCommand {
@@ -31,16 +36,32 @@ class RemapEventsCommand {
     const sourceFile = await openFile(this.sourceFilename, 'r');
     const targetFile = await openFile(this.targetFilename, 'w');
 
+    //Write header
     const headerChunk = await readChunk(sourceFile);
     const numBytesWritten = await headerChunk.write(targetFile);
     this.log(`Wrote ${numBytesWritten} bytes`);
 
+    //Write track 0 (meta data)
+    const metaTrackChunk = await readChunk(sourceFile);
+    this.log(`${metaTrackChunk.typeName} [${metaTrackChunk.length} bytes]`);
+
+    let numBytes = await this.writeTrackPreamble(targetFile, metaTrackChunk);
+    for (const event of readEvents(metaTrackChunk)) {
+      const eventBytes = await event.write(targetFile);
+      numBytes += eventBytes;
+    }
+    this.log(`Wrote ${numBytes} bytes`);
+
+    //Write track 1 (with notes)
+    const { division } = parseHeader(headerChunk);
     let trackChunk = await readChunk(sourceFile);
     while (!trackChunk.isEmpty()) {
       this.log(`${trackChunk.typeName} [${trackChunk.length} bytes]`);
-
       let totalBytes = await this.writeTrackPreamble(targetFile, trackChunk);
-      for (const event of readEvents(trackChunk)) {
+
+      const ezDrummerTrack = this.parseTrack(division, trackChunk);
+      const gmTrack = this.remapTrack(ezDrummerTrack);
+      for (const event of gmTrack.allEvents()) {
         const eventBytes = await event.write(targetFile);
         totalBytes += eventBytes;
       }
@@ -51,6 +72,18 @@ class RemapEventsCommand {
 
     await targetFile.close();
     await sourceFile.close();
+  }
+
+  private parseTrack(division: Division, trackChunk: MidiChunk): MidiTrack {
+    const track = new MidiTrackBuilder();
+    track.withDivisionInTicks(division.ticksPerQuarterNote);
+    readEvents(trackChunk).forEach((x) => track.addMidiEvent(x));
+    return track.build();
+  }
+
+  private remapTrack(sourceTrack: MidiTrack): MidiTrack {
+    const midiMap = EZDrummerMidiMap.version2Map();
+    return sourceTrack.remap(midiMap);
   }
 
   private async writeTrackPreamble(
