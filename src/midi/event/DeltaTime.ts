24b07aaa77afe5ff25b73251d03503451f479cab
Kyle Krull <kdkrull@gmail.com>
2023-10-21 23:10:26 -0500

    Implement DeltaTime#write

--- src/midi/event/DeltaTime.ts
+++ src/midi/event/DeltaTime.ts
@@ -12,7 +12,25 @@ export class DeltaTime {
     return new DeltaTime(this.ticks + other.ticks);
   }
 
-  async write(_file: FileHandle): Promise<number> {
-    throw new Error('Method not implemented.');
+  toVariableLengthQuantity(): number[] {
+    let quantity = this.ticks;
+    const bytes = [];
+
+    bytes.unshift(quantity & 0x7f);
+    quantity = quantity >> 7;
+    while (quantity > 0) {
+      const quantityBits = quantity & 0x7f;
+      const byte = quantityBits | 0x80;
+      bytes.unshift(byte);
+      quantity = quantity >> 7;
+    }
+
+    return bytes;
+  }
+
+  async write(file: FileHandle): Promise<number> {
+    const buffer = Buffer.from(this.toVariableLengthQuantity());
+    const writeQuantity = await file.write(buffer);
+    return writeQuantity.bytesWritten;
   }
 }
